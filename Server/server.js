57362514fbea0c7ffbd78db138873f51858b4053
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-04-17 09:46:35 +0530

    cors

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://expense-tracker-frontend-sand-seven.vercel.app",
     credentials: true,
   }),
 );
